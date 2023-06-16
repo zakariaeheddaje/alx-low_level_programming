de6e98f87b7e43362be7a1bc4e635c5401f4da64
zakariae11 <zakariaeheddaje@gmail.com>
2023-06-16 23:52:51 +0100

    0x01-variables_if_else_while/

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,20 +2,20 @@
 
 int main(void)
 {
-    int num;
+	int num;
 
-    for (num = 0; num < 10; num++)
-    {
-        putchar(num + '0');
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
 
-        if (num != 9)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return 0;
+	return 0;
 }
