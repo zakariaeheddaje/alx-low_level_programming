41fee687028b074c5da1ef8de00d9ac64cf44227
zakariae11 <zakariaeheddaje@gmail.com>
2023-06-16 23:30:06 +0100

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int a;
+
+	for (a = 0; a < 26; a++)
+	{
+		putchar(alphabet[a]);
+	}
+	putchar('\n');
+	return (0);
+}
