a522edc24a9a2125f7f88bb697f02abe8e99e133
zakariae11 <zakariaeheddaje@gmail.com>
2023-06-18 22:56:27 +0100

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,30 +7,27 @@
  */
 int main(void)
 {
-    int x;
-    int y;
-
-    for (x = 48; x < 57; x++)
-    {
-        for (y = 49; y < 58; y++)
-        {
-            if (x != y)
-            {
-                if (y > x)
-                {
-                    putchar(x);
-                    putchar(y);
-                    if (x != 56 || y != 57)
-                    {
-                        putchar(',');
-                        putchar(' ');
-                    }
-                }
-            }
-        }
-    }
-
-    putchar('\n');
-
-    return (0);
+int x;
+int y;
+for (x = 48; x < 57; x++)
+{
+for (y = 49; y < 58; y++)
+{
+if (x != y)
+{
+if (y > x)
+{
+putchar(x);
+putchar(y);
+if (x != 56 || y != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
