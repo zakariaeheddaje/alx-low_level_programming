1e87bbbd9e8cc9de0a56b6bcefcdcb18c7bf8875
zakariae11 <zakariaeheddaje@gmail.com>
2023-06-20 17:10:49 +0100

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - Check function
+ * @c: A parameter to be checked
+ * Description:function that checks for lowercase character
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+
+int _islower(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
