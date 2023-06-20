c276001aaf8edcc7145334a9c4d8ff7a5b981160
zakariae11 <zakariaeheddaje@gmail.com>
2023-06-20 17:05:50 +0100

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,18 +1,19 @@
 #include "main.h"
+
 /**
 * main - check the code.
 *
 * Return: Always 0.
 */
 
-int islower(int c)
+int _islower(int c)
 {
-	if (c >= "a" || c <= "z")
+	if (c >= 'a' && c <= 'z')
 	{
-		return 1;
+		return (1);
 	}
 	else
 	{
-		return 0;
+		return (0);
 	}
 }
